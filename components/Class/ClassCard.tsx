af033be4f713c0d89042c22b2a501670703f782c
Rogerio <91988248+rogerio-js@users.noreply.github.com>
2024-11-14 17:45:22 +0700

    edit themedtext

--- components/Class/ClassCard.tsx
+++ components/Class/ClassCard.tsx
@@ -3,56 +3,85 @@ import { format } from "date-fns";
 import { router } from "expo-router";
 import { Image, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { ThemedText } from "../ThemedText";
+import { Ionicons } from "@expo/vector-icons";
 
 const ClassCard = ({ classItem }: { classItem: Class }) => {
+  const isAthleteEnrolled = true;
   return (
     <TouchableOpacity
       onPress={() => router.push(`/class/${classItem.id}`)}
-      className="flex-row bg-gray-100 p-4 rounded-lg mb-4 items-center justify-between"
+      className="flex-row items-center justify-between p-4 mb-4 bg-gray-100 rounded-lg"
     >
       {/* Avatar and Details */}
       <View className="flex-row items-center flex-1">
-       <TouchableOpacity onPress={() => alert("modal profile pic")}>
-       <Image
-          source={{
-            uri:
-              classItem.coach?.profileImageUrl ||
-              require("@/assets/images/muaythai.jpg"),
-          }}
-          style={styles.avatar}
-        />
-       </TouchableOpacity>
-        <View className="items-start text-sm justify-center flex-1">
+        <TouchableOpacity onPress={() => alert("modal profile pic")}>
+          <Image
+            source={{
+              uri:
+                classItem.coach?.profileImageUrl ||
+                require("@/assets/images/muaythai.jpg"),
+            }}
+            style={styles.avatar}
+          />
+        </TouchableOpacity>
+        <View className="items-start justify-center flex-1 text-sm">
           <Text className="text-lg font-bold text-black">{classItem.name}</Text>
-          <View className="flex-row gap-x-2 items-center">
+          <View className="flex-row items-center gap-x-2">
             <ThemedText type="label">Coach:</ThemedText>
             <ThemedText type="label_description">
               {classItem.coach?.firstName} {classItem.coach?.lastName}
             </ThemedText>
           </View>
-          <View className="flex-row gap-x-2 items-center">
-            <ThemedText type="label">Spots Available:</ThemedText>
-            <ThemedText type="label_description">
-              {classItem.capacity &&
-                classItem.capacity - classItem.activeEnrollments}
-            </ThemedText>
-          </View>
-          <View className="flex-row gap-x-2 items-center">
-            <ThemedText type="label">Time:</ThemedText>
-            <ThemedText type="label_description">
-              {format(new Date(classItem.date), "p")}
-            </ThemedText>
+          <View className="flex-row items-center gap-x-2">
+            <View
+              className="flex-row items-center gap-x-2"
+              style={{
+                backgroundColor: "#D1FAE5",
+                paddingHorizontal: 4,
+                borderRadius: 10,
+              }}
+            >
+              <Ionicons name="calendar-outline" size={15} />
+              <ThemedText type="label">Spots:</ThemedText>
+              <ThemedText type="label_description">
+                {classItem.capacity &&
+                  classItem.capacity - classItem.activeEnrollments}
+              </ThemedText>
+            </View>
+            <View
+              className="flex-row items-center p-1 gap-x-2"
+              style={{
+                backgroundColor: "#d2d2d2",
+                paddingHorizontal: 4,
+                borderRadius: 10,
+              }}
+            >
+              <Ionicons name="time-outline" size={15} />
+              <ThemedText type="label_description">
+                {format(new Date(classItem.date), "p")}
+              </ThemedText>
+            </View>
           </View>
         </View>
       </View>
 
       {/* Enroll Button */}
-      <TouchableOpacity
-        style={styles.button}
-        onPress={() => alert("Button pressed")}
-      >
-        <Text style={styles.buttonText}>Enroll</Text>
-      </TouchableOpacity>
+      {!isAthleteEnrolled ? (
+        <TouchableOpacity
+          style={styles.enrolledButton}
+          onPress={() => alert("Book class")}
+          disabled
+        >
+          <Text style={styles.buttonTextEnrolled}>Enrolled</Text>
+        </TouchableOpacity>
+      ) : (
+        <TouchableOpacity
+          style={styles.enrollButton}
+          onPress={() => alert("Enroll in class")}
+        >
+          <Text style={styles.buttonText}>Enroll</Text>
+        </TouchableOpacity>
+      )}
     </TouchableOpacity>
   );
 };
@@ -63,7 +92,13 @@ const styles = StyleSheet.create({
     borderRadius: 25,
     marginRight: 12,
   },
-  button: {
+  enrolledButton: {
+    backgroundColor: "#b2b2b2", // Light green background
+    paddingVertical: 8,
+    paddingHorizontal: 16,
+    borderRadius: 8,
+  },
+  enrollButton: {
     backgroundColor: "#D1FAE5", // Light green background
     paddingVertical: 8,
     paddingHorizontal: 16,
@@ -71,6 +106,10 @@ const styles = StyleSheet.create({
   },
   buttonText: {
     color: "#065F46", // Dark green text
+    fontWeight: "bold",
+  },
+  buttonTextEnrolled: {
+    color: "#585858", // Dark green text
     fontWeight: "bold",
   },
 });
